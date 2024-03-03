d0c355c77aaf6cce9b5e494ca1f22bd5a2355598
GlennWSo <sondellglenn@gmail.com>
2024-03-03 15:00:59 +0100

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+
+async fn hello() -> Html<&'static str> {
+    "Hello, World!".into()
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello));
+
+    let linsener = tokio::net::TcpListener::bind("127.0.0.1:3333")
+        .await
+        .unwrap();
+    axum::serve(linsener, app).await.unwrap();
 }
