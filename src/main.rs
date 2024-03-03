df04dd69655df57cacc4802f408cbcccc2db0ec3
GlennWSo <sondellglenn@gmail.com>
2024-03-03 15:17:41 +0100

    init uploader

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    extract::Multipart,
+    response::Html,
+    routing::{get, post},
+    Router,
+};
+
+async fn upload_form() -> Html<&'static str> {
+    r#"<form enctype="multipart/form-data" method="POST">
+  <input type="file" id="myFile" name="filename">
+  <input type="submit">
+</form>"#
+        .into()
+}
+
+async fn upload(mut multipart: Multipart) {
+    while let Some(mut field) = multipart.next_field().await.unwrap() {
+        let name = field.name().unwrap().to_string();
+        let data = field.bytes().await.unwrap();
+        println!("Len of `{}` is {} bytes", name, data.len())
+    }
+}
 
 async fn hello() -> Html<&'static str> {
     "Hello, World!".into()
@@ -6,7 +27,10 @@ async fn hello() -> Html<&'static str> {
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(hello));
+    let app = Router::new()
+        .route("/", get(hello))
+        .route("/upload", post(upload))
+        .route("/upload", get(upload_form));
 
     let linsener = tokio::net::TcpListener::bind("127.0.0.1:3333")
         .await
